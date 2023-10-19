6be5767b993792de2be0b170b82304a8e8610c50
breslavskayairrra@gmail.com <breslavskayairrra@gmail.com>
2023-10-19 20:34:21 +0300

    Task4

---
+++ Tyuiu.BreslavskyaIV.Sprint2.Task4.V15.Test/DataServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.BreslavskyaIV.Sprint2.Task4.V15.Lib;
+
+namespace Tyuiu.BreslavskyaIV.Sprint2.Task4.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 1;
+            double res = ds.Calculate(x, y);
+            double wait = 5;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 441;
+            double res = ds.Calculate(x, y);
+            double wait = 12.384;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BreslavskyaIV.Sprint2.Task4.V15/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BreslavskyaIV.Sprint2.Task4.V15.Lib;
+
+namespace Tyuiu.BreslavskyaIV.Sprint2.Task4.V15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнила: Бреславская И. В. | ПКТб-23-2";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                *");
+            Console.WriteLine("* Тема: Тернарный оператор                                                 *");
+            Console.WriteLine("* Задание #4                                                               *");
+            Console.WriteLine("* Вариант #15                                                              *");
+            Console.WriteLine("* Выполнила: Бреславская И. В. | ПКТб-23-2                                 *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение переменной z    *");
+            Console.WriteLine("* по введенным пользователем значениям переменных X и Y.                   *");
+            Console.WriteLine("*                                                                          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("****************************************************************************");
+
+
+
+
+            Console.WriteLine("Введите значение переменной X: ");
+            double x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение переменной Y: ");
+            double y = Convert.ToInt32(Console.ReadLine());
+
+            DataService ds = new DataService();
+            double res = ds.Calculate(x, y);
+
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+
+            Console.WriteLine("Значение переменной = " + res);
+
+
+            Console.ReadKey();
+        }
+    }
+}
